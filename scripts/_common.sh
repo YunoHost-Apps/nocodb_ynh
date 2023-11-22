f7b0100e030d3ec65a9c106f57ce7aa8c86c8895
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-11-22 09:14:52 +0100

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 
 # dependencies used by the app
-nodejs_version=18
+nodejs_version=20
 
 #=================================================
 # PERSONAL HELPERS
